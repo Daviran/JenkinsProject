ebe6a3a6f8ce2ee78314ec1de67b5ea009991c2f
daviran <dbugnon@outlook.fr>
2022-04-02 11:20:39 +0200

    job tools clone repository working on jenkins, need to fix other job.

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -3,7 +3,7 @@ folder('Tools') {
 }
 job('Tools/clone-repository') {
     parameters {
-        stringParam('GIT_REPOSITORY_URL', 'Git URL of the repository to clone')
+        stringParam('GIT_REPOSITORY_URL','', 'Git URL of the repository to clone')
     }
     wrappers {
         preBuildCleanup()
@@ -13,36 +13,36 @@ job('Tools/clone-repository') {
     }
     
 }
-job('Tools/SEED') {
-    parameters {
-        stringParam('GITHUB_NAME', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
-        stringParam('DISPLAY_NAME', 'Display name for the job')
-    }
-    wrappers {
-        preBuildCleanup()
-    }
-    steps {
-        dsl {
-            text("""
-            job("$DISPLAY_NAME"){
-        scm {
-            github("$GITHUB_NAME")
-        }
-        triggers {
-            scm('* * * *')
-        }
-        wrappers {
-            preBuildCleanup()
-        }
-        steps {
-        shell('make fclean')
-        shell('make')
-        shell('make tests_run')
-        shell('make clean')
-        }
-            }""")
+// job('Tools/SEED') {
+//     parameters {
+//         stringParam('GITHUB_NAME','', 'GitHub repository owner/repo_name (e.g.: "EpitechIT31000/chocolatine")')
+//         stringParam('DISPLAY_NAME', '', 'Display name for the job')
+//     }
+//     wrappers {
+//         preBuildCleanup()
+//     }
+//     steps {
+//         dsl {
+//             text("""
+//             job("$DISPLAY_NAME"){
+//         scm {
+//             github("$GITHUB_NAME")
+//         }
+//         triggers {
+//             scm('* * * *')
+//         }
+//         wrappers {
+//             preBuildCleanup()
+//         }
+//         steps {
+//         shell('make fclean')
+//         shell('make')
+//         shell('make tests_run')
+//         shell('make clean')
+//         }
+//             }""")
         
-        }
+//         }
         
-    }
-}
+//     }
+// }
